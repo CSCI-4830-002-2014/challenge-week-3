e553d1ba53e50019665379144dbe412492038be4
Justin McBride <justin@dare599z.com>
2014-09-10 17:36:59 -0600

    Add CP 4

--- http.js
+++ http.js
@@ -1,7 +1,7 @@
 var http = require('http');
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello World\n');
+	res.end('Hello Justin\n');
 }).listen(8080);
 
 console.log('Server running on port 8080.');
